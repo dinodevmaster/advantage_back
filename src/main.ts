5dd59879b1fd9a5effff1ee8e983051043721c16
Omar G <dinodev.master@gmail.com>
2023-03-23 12:53:02 -0600

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  const options = {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        credentials: true
+      };
+  app.enableCors(options);
   await app.listen(3000);
 }
 bootstrap();

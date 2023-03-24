0294b69fb4ae9edc8c8a29ff1e9f73c6146a6ca8
Omar G <dinodev.master@gmail.com>
2023-03-24 11:31:58 -0600

    cors_v2

--- dist/main.js
+++ dist/main.js
@@ -3,15 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const options = {
-        origin: "*",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        credentials: true
-    };
-    app.enableCors(options);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        cors: true,
+    });
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
-  const options = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  };
-
-  app.enableCors(options);
-  
   await app.listen(3000);
 }
 bootstrap();

b7edf171a66eadb031ff9937e1a75fc1cd8b8f58
Omar G <dinodev.master@gmail.com>
2023-03-24 15:17:30 -0600

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'https://advantage-front.vercel.app/',
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
